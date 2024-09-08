5f8c86803723b107c6074c094aa5cb04dd5f025c
XTrivent <gungraka2002@gmail.com>
2024-09-09 07:51:28 +0800

    Update index.blade.php

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -21,7 +21,7 @@
     #hero {
         width: 100%;
         height: 100vh;
-        background: url("../img/hero-bg.jpg") top center no-repeat;
+        background: url("../landing/assets/img/20240902_134146.jpg") top center no-repeat;
         background-size: cover;
         position: relative;
         padding-top: 82px;
@@ -35,8 +35,8 @@
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
         <div class="row">
             <div class="col-xl-6">
-                <h1>Bettter digital experience with Presento</h1>
-                <h2>We are team of talented designers making websites with Bootstrap</h2>
+                <h1>Puri Agung Carangsari</h1>
+                <h2>Objek Wisata Bersejarah</h2>
             </div>
         </div>
     </div>
@@ -48,7 +48,7 @@
     <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-            <h2>Contact</h2>
+            <h2>Kontak</h2>
         </div>
 
         <div class="row" data-aos="fade-up" data-aos-delay="100">
@@ -59,22 +59,22 @@
                     <div class="col-md-12">
                         <div class="info-box">
                             <i class="bx bx-map"></i>
-                            <h3>Our Address</h3>
-                            <p>A108 Adam Street, New York, NY 535022</p>
+                            <h3>Alamat Kami</h3>
+                            <p>Jl. I.GST.NGR Rai, Desa Carangsari, Kecamatan Petang, Kabupaten Badung</p>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="info-box mt-4">
                             <i class="bx bx-envelope"></i>
-                            <h3>Email Us</h3>
-                            <p>info@example.com<br>contact@example.com</p>
+                            <h3>Email Kami</h3>
+                            <p>agungsuta02@gmail.com<br>bagussuarmandala@gmail.com</p>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="info-box mt-4">
                             <i class="bx bx-phone-call"></i>
-                            <h3>Call Us</h3>
-                            <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
+                            <h3>Hubungi Kami</h3>
+                            <p>+62 813 3875 6501<br>+62 819 9988 9930</p>
                         </div>
                     </div>
                 </div>
@@ -83,7 +83,7 @@
 
             <div class="col-lg-6">
                 <div class="google-maps">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15776.496492363074!2d115.22448505000001!3d-8.679744799999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd240f51f1412a5%3A0xa7e549b0b86dc50e!2sLapangan%20Puputan%20Renon!5e0!3m2!1sid!2sid!4v1711029335675!5m2!1sid!2sid" width="600" height="410" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3946.5141399042386!2d115.21931552579265!3d-8.44927749159071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd223fa438090c3%3A0x32c21dfde749ffdc!2sPuri%20Agung%20Carangsari!5e0!3m2!1sid!2sid!4v1725011072701!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
